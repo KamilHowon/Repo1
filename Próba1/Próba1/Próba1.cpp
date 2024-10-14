c8920f070ab341022ede284be6f20154e0fcb8c7
KamilHowon <kh311933@student.polsl.pl>
2024-10-14 09:41:43 +0200

    1.2.7

--- Próba1/Próba1/Próba1.cpp
+++ Próba1/Próba1/Próba1.cpp
@@ -2,15 +2,33 @@
 
 using namespace std;
 
-int wejscie;
+int first, second;
 
+int main()
+{
+    cout << "Podaj swoja ulubiona liczbe" << endl;
+    cin >> first;
+    second = first + 1;
+
+    cout << "Twoja ulubiona liczba po dodanu jedynki to: " << second << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+/*Zadanie 1.2.4
+* int wejscie;
 int main()
 {
     cout << "Podaj swoja ulubiona liczbe" << endl;
     cin >> wejscie;
     cout << "Twoja ulubiona liczba to: "<<wejscie << endl;
     return 0;
-}
+}*/
 
 
 
